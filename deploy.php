5111c11971d7ab0870c17d8927246bc314d05e12
Jonathan Baudoin <jonathan@francepronet.com>
2017-03-22 14:06:54 +0100

    Deployer file

---
+++ deploy.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * This file has been generated automatically.
+ * Please change the configuration for correct use deploy.
+ */
+
+require 'recipe/symfony.php';
+
+// Configure servers
+server('prod', '51.254.124.178', 22)
+    ->user('mesmenus')
+    ->password('xxx')
+    //->identityFile()
+    ->stage('prod')
+    ->env('deploy_path', '/home/mesmenus/web')
+    ->env('branch', 'master')
+    ->env('env', 'prod');
+
+/*
+server('beta', 'beta.domain.com')
+    ->user('username')
+    ->password()
+    ->env('deploy_path', '/var/www/beta.domain.com');
+*/
+
+// Set configurations
+set('repository', 'git@bitbucket.org:JonathanBaudoin/mesmenus.git');
+set('keep_releases', 5);
+set('shared_files', ['app/config/parameters.yml']);
+set('shared_dirs', [
+    'app/logs',
+    'vendor',
+    'web/bundles',
+    'web/css',
+    'web/js',
+
+]);
+set('writable_dirs', [
+    'var/cache',
+    'var/logs',
+    'web/assets',
+    'web/bundles',
+    'web/css',
+    'web/js',
+]);
+
+env('composer_options', 'update --no-dev --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction');
+
+task('vendors', function () {
+    cd('{{release_path}}');
+    run('npm install');
+    run('bower update');
+    run('php bin/console assets:install --symlink');
+    run('php bin/console assetic:dump --env=prod');
+    run('php bin/console fos:js-routing:dump --env=prod');
+});
+
+task('database', function () {
+    cd('{{release_path}}');
+    run('php app/console doctrine:schema:update --force --env=prod');
+});
+
+task('cache', function () {
+    cd('{{release_path}}');
+});
+
+after('deploy:vendors', 'vendors');
+after('cleanup', 'deploy:writable');
+after('deploy:cache:warmup', 'cache');
+before('deploy:symlink', 'database');
