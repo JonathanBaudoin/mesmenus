d2aa47cf50d359b15b8455f3c7fce799b69a61e7
Jonathan Baudoin <jonathan@francepronet.com>
2017-04-06 13:30:54 +0200

    Add ingredient from recipe

--- web/assets/js/recipe/add_ingredient.js
+++ web/assets/js/recipe/add_ingredient.js
@@ -4,8 +4,56 @@ function AddIngredient() {
 
 AddIngredient.prototype = {
     ready: function() {
+        AddIngredient.prototype.createNewIngredient();
         AddIngredient.prototype.displayIngredientOnLoad();
         AddIngredient.prototype.displayIngredientOnChange();
+    },
+
+    createNewIngredient: function() {
+        $('#add-ingredient #ingredient-button .btn').click(function(e) {
+            e.preventDefault();
+
+            var ingredientForm = $('#add-ingredient #ingredient-form');
+            ingredientForm.show('slow');
+            $(this).hide();
+
+            var contentMessage = $('#add-ingredient-message');
+            contentMessage.html('').removeClass();
+
+            $('#add-ingredient #ingredient-form .btn').click(function(e) {
+                e.preventDefault();
+
+                var ingredientName = $('#add-ingredient #ingredient-form #ingredient_name').val();
+                contentMessage.removeClass();
+
+                $.ajax({
+                    url: $('#add-ingredient #ingredient-form #ingredient_form').attr('action'),
+                    data: {
+                        'ingredientName': ingredientName
+                    },
+                    success: function(data) {
+                        contentMessage.addClass('flash-message flash-'+data.return);
+                        contentMessage.html(data.message);
+
+                        if (data.return === 'success') {
+                            $('#recipe_ingredients').append('<option value="'+data.ingredientId+'" selected="selected">'+data.ingredientName+'</option>');
+
+                            $('#added-ingredients').append(
+                                '<div id="ingredient_'+data.ingredientId+'" class="recipe-has-ingredient" data-ingredient-id="'+data.ingredientId+'"  data-ingredient-name="'+data.ingredientName+'">' +
+                                '<input class="name" name="ingredient['+data.ingredientId+'][name]" value="'+data.ingredientName+'" disabled />' +
+                                '<input type="number" required="required" class="amount" name="ingredient['+data.ingredientId+'][amount]" placeholder="Quantité" />' +
+                                '<input class="measure-unit" name="ingredient['+data.ingredientId+'][measureUnit]"  placeholder="Unité de mesure" />' +
+                                '</div>'
+                            );
+
+                            ingredientForm.hide('slow');
+                            $('#add-ingredient #ingredient-button .btn').show();
+                            $('#add-ingredient #ingredient-form #ingredient_name').val('');
+                        }
+                    }
+                })
+            });
+        });
     },
 
     displayIngredientOnLoad: function() {
